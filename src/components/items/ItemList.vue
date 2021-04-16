2a7bfb6d14942da9c913b87cd2c4d96d59fb3392
Evgenii <voznukzhenia@gmail.com>
2021-04-16 12:29:45 +0300

    save

---
+++ src/components/items/ItemList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h3>Items - loading: {{ loading }}</h3>
+    <Loader v-show="loading" />
+    <ul v-show="!loading">
+      <Item
+        v-for="item in items"
+        :key="item.id"
+        @select="onItemSelect(item)"
+        :model="item"
+      />
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { ItemInterface } from '@/models/items/item.interface';
+import Loader from '@/components/shared/Loader.vue';
+import Item from './children/Item.vue';
+
+export default defineComponent({
+  components: {
+    Item,
+    Loader,
+  },
+  props: {
+    items: {
+      type: Array as PropType<ItemInterface[]>,
+    },
+    loading: {
+      type: Boolean,
+    },
+  },
+  emits: ['selectItem'],
+  // eslint-disable-next-line vue/no-setup-props-destructure
+  setup(props, { emit }) {
+    const onItemSelect = (item: ItemInterface) => {
+      emit('selectItem', item);
+    };
+
+    return { onItemSelect };
+  },
+});
+</script>
+
+<style lang="scss">
+ul {
+  list-style-type: none;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 0px;
+}
+</style>
